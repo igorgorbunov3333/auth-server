3b192df31bc172e5ed20e2afc9795c4a98ea34ab
Ihor Horbunov <igorgorbunov3333@gmail.com>
2020-06-27 14:45:49 +0300

    remove .idea folder, remove unnecessary imports in AuthController

--- src/main/java/com/company/authserver/view/AuthController.java
+++ src/main/java/com/company/authserver/view/AuthController.java
@@ -2,14 +2,10 @@ package com.company.authserver.view;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.Past;
-
 @RestController
-//@RequestMapping("/auth1")
 public class AuthController {
 
     @GetMapping(path = "/auth1")
